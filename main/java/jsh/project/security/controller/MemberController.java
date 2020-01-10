fe1c2a94a8593e947d16b1a1d81836b513747c4f
jangsehun92 <jangsehun92@gmail.com>
2020-01-10 15:48:45 +0900

    틀 잡기

---
+++ main/java/jsh/project/security/controller/MemberController.java
@@ -0,0 +1,10 @@
+package jsh.project.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/member")
+public class MemberController {
+
+    
+
+}
